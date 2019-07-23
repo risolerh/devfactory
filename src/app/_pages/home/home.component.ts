62bb98ce906590bc3d454103096b34c5c4c95ad5
Ricardo Soler <risolerh@gmail.com>
2019-07-23 00:29:05 -0500

    efecto ocultar y cambio de color de fondo

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
