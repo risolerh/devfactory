e4a72e45f50b0d6bdf5cac1c0982596adc6eb81c
Ricardo Soler <risolerh@gmail.com>
2019-07-22 23:17:07 -0500

    se agregan efectos

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    AOS.init({
+      duration: 1500,
+      easing: 'ease-in-out-back'
+
+    });
+  }
 
   ngOnInit() {
+
+
+
   }
 
 }
