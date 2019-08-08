719dfcd8374cda8651b8d15221a2453450a18391
Ricardo Soler <risolerh@gmail.com>
2019-08-07 23:35:33 -0500

    configuraciones de banner y servicios

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, HostListener } from '@angular/core';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
@@ -10,28 +10,33 @@ import 'aos/dist/aos.css';
 })
 export class HomeComponent implements OnInit {
 
+  // tamaño de ventana
+  public innerHeight: any;
 
   public slides = [ 
-    new item(null, null, null, null),
-    new item(null, null, null, null),
-    new item(null, null, null, null) 
+    new item("slide1.titulo", "slide1.subtitulo", null, "https://previews.123rf.com/images/pumpyvector/pumpyvector1707/pumpyvector170700008/81576154-fondo-de-tecnolog%C3%ADa-abstracta.jpg"),
+    new item("slide2.titulo", "slide2.subtitulo", null, "https://www.grupovision.org/wp-content/uploads/2016/04/c-servicios.jpg"),
+    new item("slide3.titulo", "slide3.subtitulo", null, "https://media.istockphoto.com/vectors/abstract-hexagon-background-technology-polygonal-concept-vector-id996971400"),
+    new item("slide4.titulo", "slide4.subtitulo", null, "https://previews.123rf.com/images/number168/number1681507/number168150700642/42341953-vector-abstracto-futurista-ingenier%C3%ADa-rueda-de-engranaje-en-la-placa-de-circuito-ilustraci%C3%B3n-de-alta-tecn.jpg"),
+    new item("slide5.titulo", "slide5.subtitulo", null, "https://previews.123rf.com/images/number168/number1681507/number168150700603/42341230-vector-ilustraci%C3%B3n-de-la-tarjeta-de-circuitos-futurista-abstracto-fondo-de-tecnolog%C3%ADa-de-alta-ordenador-b.jpg"),
+    
   ];
  
   /*
-   *  
+   *   Servicios 
    */
   public specialties = [
-    new item("migracion.titulo"       , null, "migracion.descripcion"         , ""), 
-    new item("inovacion.titulo"       , null, "inovacion.descripcion"         , ""), 
-    new item("web.titulo"             , null, "web.descripcion"               , ""), 
-    new item("bigdata.titulo"         , null, "bigdata.descripcion"           , ""), 
-    new item("etl.titulo"             , null, "etl.descripcion"               , ""), 
-    new item("mineria.titulo"         , null, "mineria.descripcion"           , ""), 
-    new item("machinelearning.titulo" , null, "machinelearning.descripcion"   , ""), 
-    new item("internetcosas.titulo"   , null, "internetcosas.descripcion"     , ""), 
-    new item("admon.titulo"           , null, "admon.descripcion"             , ""), 
-    new item("capacitacion.titulo"    , null, "capacitacion.descripcion"      , ""), 
-    new item("consultoria.titulo"     , null, "consultoria.descripcion"       , ""), 
+    new item("web.titulo"             , null, "web.descripcion"               , "web"), 
+    new item("inovacion.titulo"       , null, "inovacion.descripcion"         , "wb_incandescent"), 
+    new item("capacitacion.titulo"    , null, "capacitacion.descripcion"      , "people"), 
+    new item("consultoria.titulo"     , null, "consultoria.descripcion"       , "wb_iridescent"), 
+    new item("migracion.titulo"       , null, "migracion.descripcion"         , "swap_horiz"), 
+    new item("etl.titulo"             , null, "etl.descripcion"               , "transform"), 
+    // new item("bigdata.titulo"         , null, "bigdata.descripcion"           , "storage"), 
+    // new item("mineria.titulo"         , null, "mineria.descripcion"           , "find_replace"), 
+    // new item("machinelearning.titulo" , null, "machinelearning.descripcion"   , "cast_for_education"), 
+    // new item("internetcosas.titulo"   , null, "internetcosas.descripcion"     , "home"), 
+    // new item("admon.titulo"           , null, "admon.descripcion"             , "developer_mode"), 
   ]
 
   public 
@@ -43,8 +48,15 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.innerHeight = window.innerWidth;
 
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerHeight = window.innerHeight;
+    console.log('--> resize: ' + this.innerHeight);
+}
 
 }
 
